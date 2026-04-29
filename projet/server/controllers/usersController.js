c173c792326889df100bbdcc31b01e79e27af478
AntoineCrsr <antoinecrauser@hotmail.com>
2026-04-29 05:59:33 +0200

    updates users: Controllerr and Model, fix mistakes in imports and modify routes for tests

--- projet/server/controllers/usersController.js
+++ projet/server/controllers/usersController.js
@@ -1,4 +1,4 @@
-const users = require('../models/coursesModel');
+/* const users = require('../models/usersModel');
 
 exports.getAllUsers = (req,res) => {
     users.getAll((err, results) => { 
@@ -36,4 +36,71 @@ exports.deleteUser = (req, res) => {
     if (err) throw err;
     res.json(results);
   })
-}
+} */
+
+  const users = require('../models/usersModel');
+
+exports.getAllUsers = (req, res) => {
+  users.getAll((err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    res.json(results);
+  });
+};
+
+exports.getUserById = (req, res) => {
+  users.getById(req.params.id, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(results[0]);
+  });
+};
+
+exports.createUser = (req, res) => {
+  users.create(req.body, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    res.status(201).json({
+      message: 'User created successfully',
+      id: results.insertId
+    });
+  });
+};
+
+exports.updateUser = (req, res) => {
+  users.update(req.params.id, req.body, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    if (results.affectedRows === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json({ message: 'User updated successfully' });
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  users.delete(req.params.id, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    if (results.affectedRows === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json({ message: 'User deleted successfully' });
+  });
+};
--- projet/server/models/usersModel.js
+++ projet/server/models/usersModel.js
@@ -1,4 +1,4 @@
-const db = require('../db');
+/* const db = require('../db');
 
 //GET all users
 exports.getAll = (callback) => {
@@ -23,4 +23,31 @@ exports.update = (id, data, callback) => {
 //DELETE a user
 exports.delete = (id, callback) => {
     db.query('DELETE FROM Users WHERE id = ?', [data], callback);
- }
+ } */
+
+    const db = require('../db');
+
+// GET all users
+exports.getAll = (callback) => {
+  db.query('SELECT * FROM Users', callback);
+};
+
+// GET one user
+exports.getById = (id, callback) => {
+  db.query('SELECT * FROM Users WHERE id = ?', [id], callback);
+};
+
+// CREATE one user
+exports.create = (data, callback) => {
+  db.query('INSERT INTO Users SET ?', data, callback);
+};
+
+// UPDATE one user
+exports.update = (id, data, callback) => {
+  db.query('UPDATE Users SET ? WHERE id = ?', [data, id], callback);
+};
+
+// DELETE one user
+exports.delete = (id, callback) => {
+  db.query('DELETE FROM Users WHERE id = ?', [id], callback);
+};
